773b2b3a7a4666431aaef33370453861047ddac9
CoderRahu1 <rahulpillai1221@gmail.com>
2025-08-22 18:20:22 +0530

    2. counter project

---
+++ counter/src/app/counter/counter.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.scss'
+})
+export class CounterComponent {
+  counter: number = 5;
+
+  // method
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement(){
+    this.counter--;
+  }
+}
