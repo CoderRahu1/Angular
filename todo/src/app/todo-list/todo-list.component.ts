db9db73f5b53aaf4d796bb1ac11b23d62e3816bc
CoderRahu1 <rahulpillai1221@gmail.com>
2025-08-23 22:35:36 +0530

    3.todo application

---
+++ todo/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  imports: [],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.scss'
+})
+export class TodoListComponent {
+  tasks: string[] = [];
+  newTask : string = '';
+  addTask(){
+    this.task.push
+  }
+
+}
